7347c98e5414f554d4c0609af01163101f63f84e
iAmGio <giogar2705@gmail.com>
2018-10-24 16:20:14 +0200

    Insert missing space

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import puzzle
 if __name__ == '__main__':
     print("Welcome to Word Search Solver.")
     date = input("Puzzle date as yyyy-mm-dd (leave empty to use today's date): ")
-    psid = input("PSID (leave empty to use the default one):")
+    psid = input("PSID (leave empty to use the default one): ")
     puzzle = puzzle.Puzzle(date=date, psid=psid)
     for word in puzzle.words:
         print(puzzle.match(word))
